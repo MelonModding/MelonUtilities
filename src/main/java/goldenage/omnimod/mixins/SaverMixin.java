f1b0712267761312b1670a48ee05dffdfd018bf2
Jakraes <70046168+Jakraes@users.noreply.github.com>
2024-06-25 01:34:26 +0100

    Added saver and a few interfaces

---
+++ src/main/java/goldenage/omnimod/mixins/SaverMixin.java
@@ -0,0 +1,16 @@
+package goldenage.omnimod.mixins;
+
+import goldenage.omnimod.saver.Saver;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class SaverMixin {
+	@Inject(method = "saveServerWorld", at = @At("TAIL"))
+	public void saveServerWorldInjection(CallbackInfo ci) {
+		Saver.saveAll();
+	}
+}
