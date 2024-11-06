123fd9c14e8cfb2891d06b3469f310bc06b8b609
WatermelonMojito <WatermelonMojitoLive@gmail.com>
2024-11-06 16:23:56 -0600

    Warp Command

---
+++ src/main/java/MelonUtilities/commands/warp/WarpCommand.java
@@ -0,0 +1,12 @@
+package MelonUtilities.commands.warp;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.core.net.command.CommandManager;
+import net.minecraft.core.net.command.CommandSource;
+
+public class WarpCommand implements CommandManager.CommandRegistry{
+	@Override
+	public void register(CommandDispatcher<CommandSource> commandDispatcher) {
+
+	}
+}
