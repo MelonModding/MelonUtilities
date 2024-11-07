3ce78b2c981689c062869229e51b46791e51086f
WatermelonMojito <WatermelonMojitoLive@gmail.com>
2024-11-07 13:17:27 -0600

    Forgot a couple of methods

--- src/main/java/MelonUtilities/mixins/gamerules/LoginPacketHandlerMixin.java
+++ src/main/java/MelonUtilities/mixins/gamerules/LoginPacketHandlerMixin.java
@@ -2,7 +2,7 @@ package MelonUtilities.mixins.gamerules;
 
 import net.minecraft.core.net.NetworkManager;
 import net.minecraft.core.net.packet.LoginPacket;
-import net.minecraft.server.net.handler.LoginPacketHandler;
+import net.minecraft.server.net.handler.PacketHandlerLogin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(
-	value = {LoginPacketHandler.class},
+	value = {PacketHandlerLogin.class},
 	remap = false
 )
 public class LoginPacketHandlerMixin {
