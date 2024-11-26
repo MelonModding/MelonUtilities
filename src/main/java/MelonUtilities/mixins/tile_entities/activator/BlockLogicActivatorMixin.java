54730846a556364a12b6cfd19f08a53dfc4dd56d
WatermelonMojito <WatermelonMojitoLive@gmail.com>
2024-11-25 20:37:15 -0600

    added activator locking and mixins

---
+++ src/main/java/MelonUtilities/mixins/tile_entities/activator/BlockLogicActivatorMixin.java
@@ -0,0 +1,28 @@
+package MelonUtilities.mixins.tile_entities.activator;
+
+import MelonUtilities.interfaces.TileEntityContainerInterface;
+import MelonUtilities.utility.MUtil;
+import MelonUtilities.utility.feedback.FeedbackHandler;
+import net.minecraft.core.block.BlockLogicActivator;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = BlockLogicActivator.class, remap = false)
+public class BlockLogicActivatorMixin {
+	@Inject(at = @At("HEAD"), method = "onBlockRightClicked", cancellable = true)
+	public void onBlockRightClickedInject(World world, int x, int y, int z, Player player, Side side, double xPlaced, double yPlaced, CallbackInfoReturnable<Boolean> cir) {
+
+		TileEntityContainerInterface iContainer = (TileEntityContainerInterface) world.getBlockEntity(x, y, z);
+		if(!MUtil.canInteractWithLock(iContainer.getIsLocked(), iContainer.getIsCommunityContainer(), iContainer.getLockOwner(), iContainer.getTrustedPlayers(), player)){
+			FeedbackHandler.error(player, "Activator is Locked!");
+			cir.setReturnValue(false);
+		} else {
+			//LogManager.logEventOpenContainer(player.uuid, world.getBlock(x, y, z), x, y, z);
+		}
+	}
+}
---
+++ src/main/java/MelonUtilities/mixins/tile_entities/activator/TileEntityActivatorMixin.java
@@ -0,0 +1,114 @@
+package MelonUtilities.mixins.tile_entities.activator;
+
+import MelonUtilities.interfaces.TileEntityContainerInterface;
+import com.mojang.nbt.tags.CompoundTag;
+import com.mojang.nbt.tags.ListTag;
+import com.mojang.nbt.tags.Tag;
+import net.minecraft.core.block.entity.TileEntityActivator;
+import net.minecraft.core.util.helper.UUIDHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Mixin(value = TileEntityActivator.class, remap = false)
+public class TileEntityActivatorMixin implements TileEntityContainerInterface {
+	@Unique
+	private boolean isLocked;
+
+	@Unique
+	private boolean isCommunityContainer;
+
+	@Unique
+	private UUID lockOwner;
+
+	@Unique
+	private final List<UUID> trustedPlayers = new ArrayList<>();
+
+	@Inject(at = @At("TAIL"), method = "writeToNBT")
+	public void writeToNBTInject(CompoundTag nbttagcompound, CallbackInfo ci){
+		nbttagcompound.putBoolean("isLocked", isLocked);
+		UUIDHelper.writeToTag(nbttagcompound, lockOwner, "lockOwner");
+		nbttagcompound.putBoolean("isCommunityContainer", isCommunityContainer);
+
+		ListTag trustedPlayers = new ListTag();
+		for(UUID uuid : this.trustedPlayers){
+			CompoundTag compoundTag = new CompoundTag();
+			UUIDHelper.writeToTag(compoundTag, uuid, "uuid");
+			trustedPlayers.addTag(compoundTag);
+		}
+		nbttagcompound.putList("trustedPlayers", trustedPlayers);
+	}
+
+	@Inject(at = @At("TAIL"), method = "readFromNBT")
+	public void readFromNBTInject(CompoundTag nbttagcompound, CallbackInfo ci){
+		isLocked = nbttagcompound.getBooleanOrDefault("isLocked", false);
+		lockOwner = UUIDHelper.readFromTag(nbttagcompound, "lockOwner");
+		isCommunityContainer = nbttagcompound.getBooleanOrDefault("isCommunityContainer", false);
+
+		ListTag tempListTag = nbttagcompound.getList("trustedPlayers");
+
+		for(Tag<?> tag : tempListTag){
+			if(tag instanceof CompoundTag){
+				CompoundTag compoundTag = (CompoundTag) tag;
+				trustedPlayers.add(UUIDHelper.readFromTag(compoundTag, "uuid"));
+			}
+		}
+	}
+
+	@Override
+	public boolean getIsLocked() {
+		return isLocked;
+	}
+
+	@Override
+	public boolean getIsCommunityContainer() {
+		return isCommunityContainer;
+	}
+
+	@Override
+	public void setIsLocked(boolean flag) {
+		isLocked = flag;
+	}
+
+	@Override
+	public void setIsCommunityContainer(boolean flag) {
+		isCommunityContainer = flag;
+	}
+
+	@Override
+	public UUID getLockOwner() {
+		return lockOwner;
+	}
+
+	@Override
+	public void setLockOwner(UUID owner) {
+		lockOwner = owner;
+	}
+
+	@Override
+	public List<UUID> getTrustedPlayers() {
+		return trustedPlayers;
+	}
+
+	@Override
+	public void setTrustedPlayers(List<UUID> trustedPlayers) {
+		this.trustedPlayers.clear();
+		this.trustedPlayers.addAll(trustedPlayers);
+	}
+
+	@Override
+	public void addTrustedPlayer(UUID uuid) {
+		trustedPlayers.add(uuid);
+	}
+
+	@Override
+	public void removeTrustedPlayer(UUID uuid) {
+		trustedPlayers.remove(uuid);
+	}
+}
