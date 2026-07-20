4a20e5e369a34ac610d3db8772b61f8546dca6c3
MelonMojito <104700587+MelonMojito@users.noreply.github.com>
2026-07-20 13:23:19 -0500

    * explosion fix * blast furnace fix

--- src/main/java/MelonUtilities/mixins/ExplosionMixin.java
+++ src/main/java/MelonUtilities/mixins/ExplosionMixin.java
@@ -2,7 +2,7 @@ package MelonUtilities.mixins;
 import MelonUtilities.interfaces.Lockable;
 import net.minecraft.core.world.Explosion;
 import net.minecraft.core.world.World;
-import net.minecraft.core.world.chunk.ChunkPosition;
+import net.minecraft.core.world.pos.TilePosc;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,8 +19,9 @@ public class ExplosionMixin {
 
 	@Redirect(method = "calculateBlocksToDestroy()V", at = @At(value = "INVOKE", target = "Ljava/util/Set;add(Ljava/lang/Object;)Z"))
 	private boolean preventClaimedDestruction(Set instance, Object e){
-		if (world.getTileEntity(((ChunkPosition) e).x, ((ChunkPosition) e).y, ((ChunkPosition) e).z) instanceof Lockable
-			&& ((Lockable) world.getTileEntity(((ChunkPosition) e).x, ((ChunkPosition) e).y, ((ChunkPosition) e).z)).getIsLocked()) {
+		TilePosc pos = (TilePosc) e;
+		if (world.getTileEntity(pos) instanceof Lockable
+			&& ((Lockable) world.getTileEntity(pos)).getIsLocked()) {
 			return false;
 		}
 		instance.add(e);
